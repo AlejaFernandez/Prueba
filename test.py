ac544428d1693e2a7d8b64fde0abd163b6e97ef1
Alejandra <alejandrafernandezg9701@gmail.com>
2021-11-23 16:28:30 -0500

    Generalizando la funcion

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-
 arg1= [{'nombre': "John", 'apellido': "Doe"}, {'nombre': "Jane", 'apellido': None}, {'nombre': "Jose", 'apellido':"Doe"}] 
 arg2= {'apellido': "Doe"}
+
+propArg2 = list(arg2.keys())
 
 def test (arg1, arg2):
     solucion = []
     for x in arg1:
-        if x["apellido"] == arg2["apellido"]:
+        if x[propArg2[0]] == arg2[propArg2[0]]:
             solucion.append(x)
     return print(solucion)
 
